65117d2b57dc2cb1211546153bab62abdaec4b45
Gefei Shen <35111787+GefeiSHEN@users.noreply.github.com>
2023-03-08 17:46:53 -0600

    Initial Commit

---
+++ SweetToothRecipe/SweetToothRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetToothRecipeApp.swift
+//  SweetToothRecipe
+//
+//  Created by Gefei Shen on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SweetToothRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
