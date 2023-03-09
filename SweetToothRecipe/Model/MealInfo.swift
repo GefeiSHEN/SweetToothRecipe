868560d9e7363757c23afed09fe8568f5f2d1669
Gefei Shen <35111787+GefeiSHEN@users.noreply.github.com>
2023-03-09 11:21:40 -0600

    fix: Update MealInfo Definition
    - Make MealInfo Conform to Codable

--- SweetToothRecipe/Model/MealInfo.swift
+++ SweetToothRecipe/Model/MealInfo.swift
@@ -10,8 +10,6 @@ import Foundation
 /**
  A `MealInfo` struct represents general information about a meal. This struct conforms to the `Identifiable` protocol, which allows it to be uniquely identified and used in list views.
 
- - Note: The `uuid` property is used to conform to the `Identifiable` protocol and provide a unique identifier for each instance of `MealInfo`. It is automatically generated using the `UUID` type.
-
  - Parameters:
     - id: An integer identifier for the meal.
     - name: The name of the meal.
@@ -19,12 +17,17 @@ import Foundation
 
  - SeeAlso: `Identifiable`
  */
-struct MealInfo: Identifiable {
-    var uuid = UUID()
-    ///Integer identifier for the meal.
-    var id : Int
+struct MealInfo: Identifiable, Codable {
+//    /Integer identifier for the meal.
+    var id : String
     ///The name of the meal.
     var name : String
     ///The URL of the thumbnail imagefor the meal.
     var thumb : URL
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "idMeal"
+        case name = "strMeal"
+        case thumb = "strMealThumb"
+    }
 }
