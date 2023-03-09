fd2b6d6fe72b442c0bac4bcab4b5da44a5cd73d0
Gefei Shen <35111787+GefeiSHEN@users.noreply.github.com>
2023-03-09 16:18:03 -0600

    feat: APIService add GetDessertDetail

--- SweetToothRecipe/Service/APIService.swift
+++ SweetToothRecipe/Service/APIService.swift
@@ -42,14 +42,49 @@ class APIService {
             throw NSError(domain: "Server Error", code: 0, userInfo: nil)
         }
         
-        var meals : [MealInfo] = []
+        var meals = [MealInfo]()
         meals = try JSONDecoder().decode(MealInfoResponse.self, from: data).meals
         
         return meals
     }
+    /**
+     Retrieves the meal detail from the MealDB API.
+        
+     - Parameters:
+        - mealId: String, the id of the meal
+     - Returns: A MealDetail objects
+     - Throws: An error if there was a problem with the network request or decoding the response.
+     */
+    func getDessertDetail(mealId: String) async throws -> MealDetail {
+        guard let url = URL(string: "\(baseURL)/lookup.php?i=\(mealId)") else {
+            throw NSError(domain: "Invalid URL", code: 0, userInfo: nil)
+        }
+        
+        let (data, response) = try await session.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NSError(domain: "Server Error", code: 0, userInfo: nil)
+        }
+        
+        do {
+            let mealDetailResponse = try JSONDecoder().decode(MealDetailResponse.self, from: data)
+            guard let mealDetail = mealDetailResponse.meals.first else {
+                throw NSError(domain: "Invalid Response", code: 0, userInfo: nil)
+            }
+            return mealDetail
+        } catch {
+            throw error
+        }
+    }
+    
 }
 
 ///Response format for getDessert()
 struct MealInfoResponse: Codable {
     var meals : [MealInfo]
 }
+
+///Response format for getDessertDetail()
+struct MealDetailResponse: Decodable {
+    var meals : [MealDetail]
+}
+
