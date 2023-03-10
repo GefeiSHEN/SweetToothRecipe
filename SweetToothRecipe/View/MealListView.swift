a0900a84ea4e8b1f0548651c1ed5faf3806f32c1
Gefei Shen <35111787+GefeiSHEN@users.noreply.github.com>
2023-03-09 19:04:48 -0600

    feat: basic MealListView

---
+++ SweetToothRecipe/View/MealListView.swift
@@ -0,0 +1,99 @@
+//
+//  ContentView.swift
+//  SweetToothRecipe
+//
+//  Created by Gefei Shen on 3/8/23.
+//
+
+import SwiftUI
+
+struct MealListView: View {
+    @StateObject var viewModel = MealListViewModel()
+    
+    var body: some View {
+        NavigationView {
+            Group {
+                if viewModel.isLoading && viewModel.desserts.count < 1 {
+                    ProgressView()
+                } else {
+                    List {
+                        ForEach(viewModel.desserts) { dessert in
+                            Text(dessert.name)
+                        }
+                    }
+                    .refreshable {
+                        viewModel.getDesserts()
+                        print("User Triggered Refresh")
+                    }
+                }
+            }
+            .navigationTitle("Desserts")
+            .onAppear {
+                viewModel.getDesserts()
+            }
+            .alert("Fetching Failed", isPresented: $viewModel.isError) {
+                Button("Dismiss") {
+                    print("User Dismissed Alert")
+                }
+                Button("Retry") {
+                    viewModel.getDesserts()
+                    print("User Dismissed Alert with Refresh")
+                }
+            } message: {
+                Text(viewModel.error?.localizedDescription ?? "An Error Occured")
+            }
+        }
+    }
+}
+
+class MealListViewModel: ObservableObject {
+    ///list of desserts sorted alphabetically
+    @Published var desserts = [MealInfo]()
+    ///wheather a network request is in progress
+    @Published var isLoading = false
+    ///the error thrown, if any
+    @Published var error: Error?
+    ///wheather encountered an error
+    @Published var isError = false
+    var api: APIService
+    
+    init(api: APIService = APIService()) {
+        self.api = api
+    }
+    
+    func getDesserts() {
+        withAnimation {
+            self.isLoading = true
+        }
+        Task {
+            do {
+                let desserts = try await self.api.getDessert()
+                let sortedDesserts = desserts.sorted(by: { $0.name < $1.name }) 
+                
+                DispatchQueue.main.async {
+                    withAnimation {
+                        self.desserts = sortedDesserts
+                    }
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    withAnimation {
+                        self.error = error
+                        self.isError = true
+                    }
+                }
+            }
+            DispatchQueue.main.async {
+                withAnimation {
+                    self.isLoading = false
+                }
+            }
+        }
+    }
+}
+
+struct MealListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealListView()
+    }
+}
